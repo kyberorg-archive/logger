b91bc5bea89e42655cf25f130bbe988f87acbe75
Alex Muravya <asm@virtalab.net>
2014-08-12 06:24:33 +0400

    Added demo tests

--- src/test/java/net/virtalab/logger/test/DemoTest.java
+++ src/test/java/net/virtalab/logger/test/DemoTest.java
@@ -5,8 +5,11 @@ import net.virtalab.logger.Log;
 import net.virtalab.logger.LogLevel;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 /**
- * Not really test, but demo
+ * Not really test, but fuzzIt
  */
 public class DemoTest {
 
@@ -19,6 +22,15 @@ public class DemoTest {
     public void demo(){
         //setup logger
         Log.init(LogLevel.TRACE);
+        //any time you can update log level
+        Log.updateCurrentLogLevel(LogLevel.DEBUG);
+        Log.t(TAG, "You never see me at your log");
+        Log.i("Updated log level is: "+Log.getCurrentLogLevel());
+        Log.i("Updated log level int representation: "+Log.getCurrentLogLevelAsInt());
+
+        //Status Quo
+        Log.updateCurrentLogLevel(LogLevel.TRACE);
+
         //if you don't happy with default color schema, you can change colors
         Log.changeMessageColor(LogLevel.DEBUG, Color.CYAN);
         Log.changeMessageColor(LogLevel.TRACE, Color.WHITE);
@@ -32,10 +44,10 @@ public class DemoTest {
         Log.setTimestampFormat(newTSFormat);
         Log.debug("Changing timestamp to format "+newTSFormat);
 
-        Log.i("For your information: at next line you will be warned");
-        Log.w("Just a word of warning");
-        Log.e("Error line");
-        Log.wtf("What a terrible fail! Read the manual");
+        Log.i(TAG, "For your information: at next line you will be warned");
+        Log.w(TAG, "Just a word of warning");
+        Log.e(TAG, "Error line");
+        Log.wtf(TAG, "What a terrible fail! Read the manual");
 
         //assume that we don't want to see timestamp anymore
         Log.noTime();
@@ -43,6 +55,10 @@ public class DemoTest {
         Log.i("Look ma, there is no timestamp at this line (and below)!");
         Log.info("Methods Log.info() and Log.i() have same effect");
 
+        Log.i("Same situation with methods: ");
+        Log.e("Log.e()");
+        Log.err("Log.err()");
+        Log.error("Log.error()");
         //assume that we don't want to see class name as well
         Log.noClassName();
 
@@ -52,6 +68,19 @@ public class DemoTest {
         }catch (ArithmeticException ae){
             Log.e(TAG,ae);
         }
+
+        //There is ability to change Stream for certain level
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        PrintStream errStream = new PrintStream(os);
+        Log.changeStreamForLevel(LogLevel.ERROR,errStream);
+        Log.e("Err 1");
+        Log.error(" ");
+        Log.err("Err 2");
+        String errors = os.toString();
+        Log.i("We have caught following errors: "+errors);
+        //Status Quo
+        Log.changeStreamForLevel(LogLevel.ERROR, System.err);
+
         System.out.println("THE END. Thank you for been with us!");
     }
 }
---
+++ src/test/java/net/virtalab/logger/test/FuzzingLogTest.java
@@ -0,0 +1,79 @@
+package net.virtalab.logger.test;
+
+import net.virtalab.logger.Color;
+import net.virtalab.logger.Log;
+import net.virtalab.logger.LogLevel;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Not really test, but fuzzIt
+ */
+public class FuzzingLogTest {
+
+    public static final String TAG = null;
+
+    /**
+     * Demo
+     */
+    @Test
+    public void fuzzIt(){
+        //setup logger
+        Log.init(null);
+        //expecting default level here
+        LogLevel lvl = Log.getCurrentLogLevel();
+        Assert.assertEquals(LogLevel.OFF, lvl);
+
+        //setting log level we happy with
+        Log.updateCurrentLogLevel(null); //ha-ha, it is another test by the way
+        LogLevel lvl1 = Log.getCurrentLogLevel();
+        Assert.assertEquals(LogLevel.OFF, lvl1);
+
+        //Now really set it
+        Log.updateCurrentLogLevel(LogLevel.TRACE);
+        LogLevel lvl2 = Log.getCurrentLogLevel();
+        Assert.assertEquals(LogLevel.TRACE,lvl2);
+
+        //if you don't happy with default color schema, you can change colors
+        Log.changeMessageColor(null, Color.CYAN);
+        Log.changeMessageColor(LogLevel.DEBUG, null);
+        Log.changeMessageColor(LogLevel.DEBUG, Color.CYAN);
+
+        //following two lines we don't want to see
+        Log.t(null);
+        Log.d(TAG, "This is debug message with Tag (Prefix)");
+
+        //Changing timestamp format...to NULL
+        String newTSFormat = null;
+        Log.setTimestampFormat(newTSFormat);
+        Log.debug("Changing timestamp to format "+newTSFormat);
+
+        //if we see default TS, test passed
+
+        Log.i("");
+        Log.w(null);
+        Log.e(TAG,"");
+        Log.wtf("");
+
+        //assume that we don't want to see timestamp anymore
+        Log.noTime();
+
+        //assume that we don't want to see class name as well
+        Log.noClassName();
+
+        Log.info("Let's test exceptions");
+        try{
+            int a = 5/0;
+        }catch (ArithmeticException ae){
+            Log.e(TAG,ae);
+        }
+
+        Log.changeStreamForLevel(LogLevel.ERROR,null);
+        Log.changeStreamForLevel(null,null);
+
+        Log.changeStreamForLevel(LogLevel.ERROR,System.err);
+
+
+        System.out.println("THE END. Thank you for been with us!");
+    }
+}
